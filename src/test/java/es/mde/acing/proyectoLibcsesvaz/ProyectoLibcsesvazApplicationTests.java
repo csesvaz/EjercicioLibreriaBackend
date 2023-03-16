ce4510c6f6fda29f7957c36d86c743e3a3337e31
camilocococ@gmail.com <camilocococ@gmail.com>
2023-03-16 12:03:09 +0100

    primer commit

---
+++ src/test/java/es/mde/acing/proyectoLibcsesvaz/ProyectoLibcsesvazApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyectoLibcsesvaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLibcsesvazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
